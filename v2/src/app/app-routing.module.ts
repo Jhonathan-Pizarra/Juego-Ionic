d6db6ad09cb679865d753a1be184bf4587efd789
Jhonathan-Pizarra <jhonnathan.pizarra@epn.edu.ec>
2020-08-17 19:34:44 -0500

    version 2

--- v2/src/app/app-routing.module.ts
+++ v2/src/app/app-routing.module.ts
@@ -11,6 +11,10 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+     path: 'dashboard',
+     loadChildren: './dashboard/dashboard.module#DashboardPageModule'
+   },
 ];
 
 @NgModule({
